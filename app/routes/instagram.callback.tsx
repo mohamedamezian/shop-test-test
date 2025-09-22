f681ac2f568c0bacd1aed8607ea89f31bf0ab356
Mohamed Amezian <mohamed-biljonair@live.nl>
2025-09-22 12:36:13 +0200

    testtest

--- app/routes/instagram.callback.tsx
+++ app/routes/instagram.callback.tsx
@@ -7,15 +7,16 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
   if (!code) {
     return new Response("Missing code from Instagram", { status: 400 });
   }
+  const tokenUrl = `https://graph.facebook.com/v23.0/oauth/access_token?client_id=${process.env.FACEBOOK_APP_ID}&redirect_uri=${encodeURIComponent(process.env.FACEBOOK_REDIRECT_URI!)}&client_secret=${process.env.FACEBOOK_APP_SECRET}&code=${code}`;
 
   // Exchange code for access token
-  const res = await fetch("https://api.instagram.com/oauth/access_token", {
+  const res = await fetch(tokenUrl, {
     method: "POST",
     body: new URLSearchParams({
-      client_id: process.env.INSTAGRAM_CLIENT_ID!,
-      client_secret: process.env.INSTAGRAM_CLIENT_SECRET!,
+      client_id: process.env.FACEBOOK_APP_ID!,
+      client_secret: process.env.FACEBOOK_APP_SECRET!,
       grant_type: "authorization_code",
-      redirect_uri: process.env.INSTAGRAM_REDIRECT_URI!,
+      redirect_uri: process.env.FACEBOOK_REDIRECT_URI!,
       code,
     }),
   });

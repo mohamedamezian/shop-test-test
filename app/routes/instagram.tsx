cb499918f490ca0a70426939e5d2496736aed744
Mohamed Amezian <mohamed-biljonair@live.nl>
2025-09-23 14:31:04 +0200

    New embed url

--- app/routes/instagram.tsx
+++ app/routes/instagram.tsx
@@ -6,9 +6,7 @@ export const loader: LoaderFunction = async () => {
   const redirectUri = process.env.INSTAGRAM_REDIRECT_URI!;
   const scope = process.env.INSTA_SCOPES || "instagram_basic";
 
-  const authUrl = `https://www.instagram.com/oauth/authorize?force_reauth=true&client_id=${appId}&redirect_uri=${encodeURIComponent(
-    redirectUri
-  )}&response_type=code&scope=${scope}`;
+  const authUrl = `https://www.instagram.com/oauth/authorize?force_reauth=true&client_id=1565375811115462&redirect_uri=https://shop-test-test.vercel.app/instagram/callback&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish%2Cinstagram_business_manage_insights`;
 
   console.log("Facebook OAuth URL:", authUrl);
   console.log("App ID:", appId);

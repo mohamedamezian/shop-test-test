0a8ea54249b4591272d348f466beb12c49a97967
Mohamed Amezian <mohamed-biljonair@live.nl>
2025-09-23 14:40:20 +0200

    hardcoced variables

--- app/routes/instagram.callback.tsx
+++ app/routes/instagram.callback.tsx
@@ -8,16 +8,16 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
     return new Response("Missing code from Instagram", { status: 400 });
   }
 
-  const tokenUrl = `https://graph.instagram.com/v23.0/oauth/access_token?client_id=${process.env.INSTAGRAM_APP_ID}&redirect_uri=${encodeURIComponent(process.env.INSTAGRAM_REDIRECT_URI!)}&client_secret=${process.env.INSTAGRAM_APP_SECRET}&code=${code}`;
+  const tokenUrl = `https://graph.instagram.com/v23.0/oauth/access_token?client_id=1565375811115462&redirect_uri=https://shop-test-test.vercel.app/instagram/callback&client_secret=f43bd7853db5f37a3b17356699ecab36&code=${code}`;
 
   // Exchange code for access token
   const res = await fetch(tokenUrl, {
     method: "POST",
     body: new URLSearchParams({
-      client_id: process.env.INSTAGRAM_APP_ID!,
-      client_secret: process.env.INSTAGRAM_APP_SECRET!,
+      client_id: "1565375811115462",
+      client_secret: "f43bd7853db5f37a3b17356699ecab36",
       grant_type: "authorization_code",
-      redirect_uri: process.env.INSTAGRAM_REDIRECT_URI!,
+      redirect_uri: "https://shop-test-test.vercel.app/instagram/callback",
       code,
     }),
   });

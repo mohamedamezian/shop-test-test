a2e53245a41a72a29b94ee9457f7e9cd67c45e73
Mohamed Amezian <mohamed-biljonair@live.nl>
2025-09-23 14:04:19 +0200

    Instagram button in the main route

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,13 +1,15 @@
 import { AppProvider } from "@shopify/polaris";
 import translations from "@shopify/polaris/locales/en.json";
 import { FacebookConnectButton } from "../components/FacebookConnectButton";
+import { InstagramConnectButton } from "../components/InstagramConnectButton";
 
 export default function Index() {
   return (
     <AppProvider i18n={translations}>
       <div style={{ padding: "2rem" }}>
         <h1>Welcome to your app 🎉</h1>
-        <FacebookConnectButton />
+        <FacebookConnectButton />   
+        <InstagramConnectButton />
       </div>
     </AppProvider>
   );

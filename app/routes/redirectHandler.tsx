daaacfa151eb1526cd46debc4e744dac3d0ff008
Mohamed Amezian <mohamed-biljonair@live.nl>
2025-09-25 10:16:07 +0200

    Removed linebreaks

--- app/routes/redirectHandler.tsx
+++ app/routes/redirectHandler.tsx
@@ -9,22 +9,13 @@ export const loader: LoaderFunction = async ({request}) => {
     const FBappId = process.env.FACEBOOK_APP_ID!;
     const FBredirectUri = process.env.FACEBOOK_REDIRECT_URI!;
     const FBscope = process.env.FACEBOOK_SCOPES!;
-    const FBauthUrl = `https://www.facebook.com/v23.0/dialog/oauth
-    ?client_id=${FBappId}
-    &redirect_uri=${encodeURIComponent(FBredirectUri)}
-    &scope=${FBscope}
-    &response_type=code`;
+    const FBauthUrl = `https://www.facebook.com/v23.0/dialog/oauth?client_id=${FBappId}&redirect_uri=${encodeURIComponent(FBredirectUri)}&scope=${FBscope}&response_type=code`;
 
     //Instagram variables
     const IGappId = process.env.INSTAGRAM_APP_ID!;
     const IGredirectUri = process.env.INSTAGRAM_REDIRECT_URI!;
     const IGscope = process.env.INSTA_SCOPES!;
-    const IGauthUrl = `https://www.instagram.com/oauth/authorize
-    ?force_reauth=true
-    &client_id=${IGappId}
-    &redirect_uri=${encodeURIComponent(IGredirectUri)}
-    &response_type=code
-    &scope=${encodeURIComponent(IGscope)}`;
+    const IGauthUrl = `https://www.instagram.com/oauth/authorize?force_reauth=true&client_id=${IGappId}&redirect_uri=${encodeURIComponent(IGredirectUri)}&response_type=code&scope=${encodeURIComponent(IGscope)}`;
 
     return type === "facebook" ? redirect(FBauthUrl + `&type=${type}`) : redirect(IGauthUrl + `&type=${type}`);
 

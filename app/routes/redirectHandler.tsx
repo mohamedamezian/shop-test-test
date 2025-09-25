c06d5ed63fabbc939c3d5f7c51f60c1d83adeae1
Mohamed Amezian <mohamed-biljonair@live.nl>
2025-09-25 10:35:11 +0200

    Removed redundant callbacks

--- app/routes/redirectHandler.tsx
+++ app/routes/redirectHandler.tsx
@@ -7,13 +7,13 @@ export const loader: LoaderFunction = async ({request}) => {
 
     //Facebook variables
     const FBappId = process.env.FACEBOOK_APP_ID!;
-    const FBredirectUri = process.env.FACEBOOK_REDIRECT_URI!;
+    const FBredirectUri = process.env.REDIRECT_URI!;
     const FBscope = process.env.FACEBOOK_SCOPES!;
     const FBauthUrl = `https://www.facebook.com/v23.0/dialog/oauth?client_id=${FBappId}&redirect_uri=${encodeURIComponent(FBredirectUri)}&scope=${encodeURIComponent(FBscope)}&response_type=code`;
 
     //Instagram variables
     const IGappId = process.env.INSTAGRAM_APP_ID!;
-    const IGredirectUri = process.env.INSTAGRAM_REDIRECT_URI!;
+    const IGredirectUri = process.env.REDIRECT_URI!;
     const IGscope = process.env.INSTA_SCOPES!;
     const IGauthUrl = `https://www.instagram.com/oauth/authorize?force_reauth=true&client_id=${IGappId}&redirect_uri=${encodeURIComponent(IGredirectUri)}&response_type=code&scope=${encodeURIComponent(IGscope)}`;
 

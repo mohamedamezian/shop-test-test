53753936d99f60a5fee10fef97902ffce5abb6d0
Mohamed Amezian <mohamed-biljonair@live.nl>
2025-09-25 10:56:08 +0200

    redirect uri changes

--- app/routes/redirectHandler.callback.tsx
+++ app/routes/redirectHandler.callback.tsx
@@ -4,13 +4,14 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
   const url = new URL(request.url);
   const code = url.searchParams.get("code");
   const type = url.searchParams.get("type");
+  const baseRedirectUri = process.env.REDIRECT_URI!;
 
   if( type === "facebook") {
     // Facebook token exchange logic  
     if (!code) {
       return new Response("Missing code from Facebook", { status: 400 });
     }
-      const tokenUrl = `https://graph.facebook.com/v23.0/oauth/access_token?client_id=${process.env.FACEBOOK_APP_ID}&redirect_uri=${encodeURIComponent(process.env.REDIRECT_URI!)}&client_secret=${process.env.FACEBOOK_APP_SECRET}&code=${code}`;
+      const tokenUrl = `https://graph.facebook.com/v23.0/oauth/access_token`;
       // Exchange code for access token
   const res = await fetch(tokenUrl, {
     method: "POST",
@@ -18,7 +19,7 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
       client_id: process.env.FACEBOOK_APP_ID!,
       client_secret: process.env.FACEBOOK_APP_SECRET!,
       grant_type: "authorization_code",
-      redirect_uri: process.env.REDIRECT_URI!,
+      redirect_uri: `${baseRedirectUri}?type=${type}`,
       code,
     }),
   });
@@ -40,7 +41,7 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
       client_id: process.env.INSTAGRAM_APP_ID!,
       client_secret: process.env.INSTAGRAM_APP_SECRET!,
       grant_type: "authorization_code",
-      redirect_uri: process.env.REDIRECT_URI!,
+      redirect_uri: `${baseRedirectUri}?type=${type}`,
       code,
     }),
   });
--- app/routes/redirectHandler.tsx
+++ app/routes/redirectHandler.tsx
@@ -4,16 +4,17 @@ import { LoaderFunction, redirect } from "@remix-run/node";
 export const loader: LoaderFunction = async ({request}) => {
     const url = new URL(request.url);
     const type = url.searchParams.get("type");
+    const baseRedirectUri = process.env.REDIRECT_URI!;
 
     //Facebook variables
     const FBappId = process.env.FACEBOOK_APP_ID!;
-    const FBredirectUri = process.env.REDIRECT_URI!;
+    const FBredirectUri = `${baseRedirectUri}?type=facebook`;
     const FBscope = process.env.FACEBOOK_SCOPES!;
     const FBauthUrl = `https://www.facebook.com/v23.0/dialog/oauth?client_id=${FBappId}&redirect_uri=${encodeURIComponent(FBredirectUri)}&scope=${encodeURIComponent(FBscope)}&response_type=code`;
 
     //Instagram variables
     const IGappId = process.env.INSTAGRAM_APP_ID!;
-    const IGredirectUri = process.env.REDIRECT_URI!;
+    const IGredirectUri = `${baseRedirectUri}?type=instagram`;
     const IGscope = process.env.INSTA_SCOPES!;
     const IGauthUrl = `https://www.instagram.com/oauth/authorize?force_reauth=true&client_id=${IGappId}&redirect_uri=${encodeURIComponent(IGredirectUri)}&response_type=code&scope=${encodeURIComponent(IGscope)}`;
 

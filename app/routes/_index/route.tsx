82b0fd4a4c0afce8d1e91eb32083a98980cc4743
Mohamed Amezian <mohamed-biljonair@live.nl>
2025-09-30 13:13:56 +0200

    Removed unified button

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -2,15 +2,15 @@ import { AppProvider } from "@shopify/polaris";
 import translations from "@shopify/polaris/locales/en.json";
 import { FacebookConnectButton } from "../components/FacebookConnectButton";
 import { InstagramConnectButton } from "../components/InstagramConnectButton";
-import { ConnectButton } from "../components/ConnectButton";
+
 
 export default function Index() {
   return (
     <AppProvider i18n={translations}>
       <div style={{ padding: "2rem" }}>
         <h1>Welcome to your app 🎉</h1>
-        <ConnectButton type="facebook" />
-        <ConnectButton type="instagram" />
+        <FacebookConnectButton />
+        <InstagramConnectButton />
       </div>
     </AppProvider>
   );

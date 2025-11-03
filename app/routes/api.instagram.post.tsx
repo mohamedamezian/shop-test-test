5f227789d728f001d01892814c0ae61124de20fe
Mohamed Amezian <mohamed-biljonair@live.nl>
2025-11-03 12:00:02 +0100

    Created new metaobject definition and updates post api to upload post files to shopify with duplication logic

--- app/routes/api.instagram.post.tsx
+++ app/routes/api.instagram.post.tsx
@@ -24,8 +24,10 @@ export interface InstagramPost {
 }
 
 
+
+
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-    const { session } = await authenticate.admin(request);
+    const { session, admin } = await authenticate.admin(request);
       // Get Instagram account
     const account = await prisma.socialAccount.findUnique({
         where: {
@@ -39,13 +41,129 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
     if(!account){
         return { error: 'No Instagram account connected' };
     }
-    // Fetch posts from Instagram Graph API
-    const posts = await fetch(`https://graph.instagram.com/me/media?fields=id,media_type,media_url,thumbnail_url,permalink,caption,timestamp,username,children{media_url,media_type}&access_token=${account.accessToken}`);
-    const postsData = await posts.json();
 
-    // Return json met posts
-    return {
-        posts: postsData.data as InstagramPost[],
-        shop: session.shop
+    const existingFileQuery = await admin.graphql(
+        `#graphql
+        query {
+            files(first:50, query: "instagram_post_") {
+                edges {
+                    node {
+                        ... on MediaImage {
+                            alt
+                            image {
+                                url
+                            }          
+                        }
+                    }
+                }
+            }
+        }
+        `
+    )
+
+    const existingFilesJson = await existingFileQuery.json();
+
+    const existingKeys = new Set(
+        existingFilesJson.data.files.edges.map((e:any) =>
+        e.node.alt.replace('instagram_post_','')
+    )
+);
+
+    // Fetch posts from Instagram Graph API
+    const igResponse = await fetch(`https://graph.instagram.com/me/media?fields=id,media_type,media_url,thumbnail_url,permalink,caption,timestamp,username,children{media_url,media_type}&access_token=${account.accessToken}`);
+    const igData = await igResponse.json();
+    const posts = igData.data as InstagramPost[];
+    const uploadResults = [];
+
+    for (const post of posts) {
+        const uniqueKey = post.id;
+        if (existingKeys.has(uniqueKey)) {
+            // Skip already uploaded posts
+            console.log(`⏭️  Skipping already uploaded post: ${post.id}`);
+            continue;
+        }
+        // Normal post upload
+        if(post.media_type !== "CAROUSEL_ALBUM"){
+            const postResponse = await admin.graphql(
+                `#graphql
+                mutation fileCreate($files: [FileCreateInput!]!) {
+                    fileCreate(files: $files) {
+                        files {
+                            id
+                            fileStatus
+                            alt
+                            createdAt
+                            ... on MediaImage {
+                                image {
+                                    width
+                                    height
+                                    url
+                                }
+                            }
+                        }
+                        userErrors {
+                            field
+                            message
+                        }
+                    }
+                }`,{
+                    variables:{
+                        files: [
+                            {
+                            alt: `instagram_post_${uniqueKey}`,
+                            contentType: "IMAGE",
+                            originalSource: post.media_url,
+                            filename: `instagram_post_${uniqueKey}.jpg`
+                        }
+                        ]
+                    },
+                }
+               );
+               const json = await postResponse.json();
+               uploadResults.push(json.data);
+        }
+
+        else{
+            for(const child of post.children?.data || []){
+                const carouselResponse = await admin.graphql(
+                    `#graphql
+                    mutation fileCreate($files: [FileCreateInput!]!) {
+                        fileCreate(files: $files) {
+                            files {
+                                id
+                                fileStatus
+                                alt
+                                createdAt
+                                ... on MediaImage {
+                                    image {
+                                        width
+                                        height
+                                        url
+                                    }
+                                }
+                            }
+                            userErrors {
+                                field
+                                message
+                            }
+                        }
+                    }`,{
+                        variables:{
+                            files: [
+                                {
+                                alt: `instagram_post_${uniqueKey}`,
+                                contentType: "IMAGE",
+                                originalSource: child.media_url,
+                                filename: `instagram_post_${uniqueKey}.jpg`
+                            }
+                            ]
+                        },
+                    }
+                   );
+                   const json = await carouselResponse.json();
+                   uploadResults.push(json.data);
+            }
+        }
     }
-};
+    return { posts, uploadResults };
+}

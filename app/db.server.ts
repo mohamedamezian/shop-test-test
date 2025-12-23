be40bea9fe23be01f118be9249353955e3076308
Mohamed Amezian <mohamed-biljonair@live.nl>
2025-12-23 12:02:51 +0100

    DB update

--- app/db.server.ts
+++ app/db.server.ts
@@ -4,12 +4,28 @@ declare global {
   var prismaGlobal: PrismaClient;
 }
 
+const datasourceUrl = process.env.PRISMA_DATABASE_URL;
+
 if (process.env.NODE_ENV !== "production") {
   if (!global.prismaGlobal) {
-    global.prismaGlobal = new PrismaClient();
+    global.prismaGlobal = new PrismaClient({
+      datasources: {
+        db: {
+          url: datasourceUrl,
+        },
+      },
+    });
   }
 }
 
-const prisma = global.prismaGlobal ?? new PrismaClient();
+const prisma =
+  global.prismaGlobal ??
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: datasourceUrl,
+      },
+    },
+  });
 
 export default prisma;
